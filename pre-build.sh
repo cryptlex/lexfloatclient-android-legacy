3a33000ded7d0a0920c523299191bcf56e62cdbb
Azan <muftiazan@gmail.com>
2021-04-14 22:51:03 +0530

    Shell Script Fix

--- pre-build.sh
+++ pre-build.sh
@@ -12,13 +12,13 @@ unzip android-armv7.jar -d ~/jni
 cp ~jni/libjnidispatch.so lexfloatclient/src/main/jniLibs/armeabi-v7a
 cp ~jni64/libjnidispatch.so lexfloatclient/src/main/jniLibs/arm64-v8a
 
-cp ~/android/libs/clang/arm64-v8a/libLexActivator.so lexfloatclient/src/main/jniLibs/arm64-v8a
-cp ~/android/libs/clang/armeabi-v7a/libLexActivator.so lexfloatclient/src/main/jniLibs/armeabi-v7a
+cp ~/android/libs/clang/arm64-v8a/libLexFloatClient.so lexfloatclient/src/main/jniLibs/arm64-v8a
+cp ~/android/libs/clang/armeabi-v7a/libLexFloatClient.so lexfloatclient/src/main/jniLibs/armeabi-v7a
 
 cp $ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/libc++_shared.so lexfloatclient/src/main/jniLibs/arm64-v8a
 cp $ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/arm-linux-androideabi/libc++_shared.so lexfloatclient/src/main/jniLibs/armeabi-v7a
 
-rm -f LexActivator-Linux.zip
+rm -f LexFloatClient-Linux.zip
 rm -f android-armv7.jar
 rm -f android-aarch64.jar
 rm -r ~/jni ~/android ~/jni64
